b61057c1e4d555b98e54fc104403633fc89141b6
zOmArRD <omarsena25@gmail.com>
2022-08-06 00:13:57 -0500

    Add SelectAsync.php

---
+++ src/MySQL/Query/SelectAsync.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Created by PhpStorm.
+ *
+ * User: zOmArRD
+ * Date: 6/8/2022
+ *
+ *
+ *        $$$$$$\  $$\                             $$\     $$\           $$\      $$\  $$$$$$\
+ *       $$  __$$\ $$ |                            $$ |    $$ |          $$$\    $$$ |$$  __$$\
+ *       $$ /  \__|$$$$$$$\   $$$$$$\   $$$$$$$\ $$$$$$\   $$ |$$\   $$\ $$$$\  $$$$ |$$ /  \__|
+ *       $$ |$$$$\ $$  __$$\ $$  __$$\ $$  _____|\_$$  _|  $$ |$$ |  $$ |$$\$$ $$ |$$ |
+ *       $$ |\_$$ |$$ |  $$ |$$ /  $$ |\$$$$\    $$ |    $$ |$$ |  $$ |$$ \$  $$ |$$ |
+ *       $$ |  $$ |$$ |  $$ |$$ |  $$ | \____$$\   $$ |$$\ $$ |$$ |  $$ |$$ |\$  /$$ |$$ |  $$\
+ *       \$$$$  |$$ |  $$ |\$$$$  |$$$$$$$  |  \$$  |$$ |\$$$$$ |$$ | \_/ $$ |\$$$$  |
+ *        \______/ \__|  \__| \______/ \_______/    \____/ \__| \____$$ |\__|     \__| \______/
+ *                                                             $$\   $$ |
+ *                                                             \$$$$  |
+ *                                                              \______/
+ *
+ *  Copyright © 2022 GhostlyMC Network (omar@ghostlymc.live) - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace GhostlyMC\Database\MySQL\Query;
+
+use mysqli;
+
+class SelectAsync extends AsyncQuery
+{
+
+    public function query(mysqli $mysqli): void
+    {
+        // TODO: Implement query() method.
+    }
+}
